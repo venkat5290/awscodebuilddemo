e6db967a3cef323f4c85077beb6edf9ad90511e4
venkat5290 <injarapu.venkat@gmail.com>
2021-06-12 23:50:34 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Flask App</h1>"
+    return "<h1>Hurray Cogratulations Succesfully Created CI/CD PIPELINE</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
